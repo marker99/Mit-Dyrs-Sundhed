55707a713ffeee62dc2034b336f760089ff782fc
Frederik <frederik.m.hansen@gmail.com>
2022-03-31 21:41:53 +0200

    fået gjort lidt rent

--- app/src/main/java/com/github/marker99/ui/home/HomeFragment.java
+++ app/src/main/java/com/github/marker99/ui/home/HomeFragment.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.github.marker99.R;
 import com.github.marker99.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+    private Button button;
 
     private FragmentHomeBinding binding;
 
@@ -25,7 +30,16 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
+
+        //alternative to findViewById
+        button = binding.buttonAddPetInfo;
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        button.setOnClickListener(view -> {
+            NavHostFragment.findNavController(this).navigate(R.id.action_nav_home_to_addPetInfoFragment);
+        });
+
         return root;
     }
 
