ef380debd0755958b065beff2dea83a8ecee7b86
Frederik <frederik.m.hansen@gmail.com>
2022-04-06 14:36:18 +0200

    man kan tilføje et meget simpel version af sundhedseftersyn nu

--- app/src/main/java/com/github/marker99/persistence/Database.java
+++ app/src/main/java/com/github/marker99/persistence/Database.java
@@ -5,11 +5,12 @@ import android.content.Context;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.github.marker99.persistence.health_inspection.HealthInspection;
 import com.github.marker99.persistence.health_inspection.HealthInspectionDAO;
 import com.github.marker99.persistence.pet.Pet;
 import com.github.marker99.persistence.pet.PetDAO;
 
-@androidx.room.Database(entities = {Pet.class}, version = 1)
+@androidx.room.Database(entities = {Pet.class, HealthInspection.class}, version = 2)
 public abstract class Database extends RoomDatabase {
 
     private static Database instance;
