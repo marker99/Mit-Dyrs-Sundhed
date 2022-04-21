741122122fbcba8d9a2bd01e02aedd37fe5f66c4
Sebastian <280301@via.dk>
2022-04-21 13:50:58 +0200

    Queries

--- app/src/main/java/com/github/marker99/webservice/DogAPI.java
+++ app/src/main/java/com/github/marker99/webservice/DogAPI.java
@@ -7,6 +7,12 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface DogAPI {
+    //API - https://docs.thedogapi.com/api-reference/breeds/breeds-search
+    //til makker
+    // https://api.thedogapi.com/v1/images/search?breed_name=Affenpinscher
+
+    //ID VIRKER HVER GANG!!
+    //https://api.thedogapi.com/v1/images/search?breed_id=1
 
     //TODO: Why is this not needed - Because website doesn't want you to use API key! ;)
     //@Headers({"x-api-key: " + "a91299bb-d87d-4eee-8617-b51535425ae8"})
