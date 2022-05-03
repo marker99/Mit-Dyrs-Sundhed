efe80b940b1a0bc9cac365a2b74005dc10c5cd2c
Frederik <frederik.m.hansen@gmail.com>
2022-05-03 17:00:29 +0200

    se hvilken user er logget ind

--- app/src/main/java/com/github/marker99/ui/SettingsFragment.java
+++ app/src/main/java/com/github/marker99/ui/SettingsFragment.java
@@ -12,29 +12,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
-import com.github.marker99.R;
-import com.github.marker99.databinding.FragmentHomeBinding;
+
 import com.github.marker99.databinding.SettingsFragmentBinding;
 
 public class SettingsFragment extends Fragment {
 
     private Button signOutButton;
+    private TextView userIdText;
 
     private SettingsFragmentBinding binding;
-
+    private SettingsViewModel settingsViewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        SettingsViewModel settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
+        settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
 
         binding = SettingsFragmentBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         bindings();
+        checkIfSignedIn();
 
         signOutButton.setOnClickListener(view -> {
             settingsViewModel.signOut();
@@ -43,9 +44,19 @@ public class SettingsFragment extends Fragment {
         return root;
     }
 
+    private void checkIfSignedIn() {
+        settingsViewModel.getCurrentUser().observe(getViewLifecycleOwner(), user -> {
+            if (user != null) {
+                String message = "User: " + user.getDisplayName();
+                userIdText.setText(message);
+            }
+        });
+    }
 
     private void bindings() {
         signOutButton = binding.signOutButton;
+        userIdText = binding.userText;
     }
+
 
 }
--- app/src/main/java/com/github/marker99/ui/SettingsViewModel.java
+++ app/src/main/java/com/github/marker99/ui/SettingsViewModel.java
@@ -4,8 +4,10 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 
 import com.github.marker99.login_firebase.data.UserRepository;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingsViewModel extends AndroidViewModel {
 
@@ -15,6 +17,10 @@ public class SettingsViewModel extends AndroidViewModel {
         super(application);
 
         userRepository = UserRepository.getInstance(application);
+    }
+
+    public LiveData<FirebaseUser> getCurrentUser() {
+        return userRepository.getCurrentUser();
     }
 
     public void signOut() {
